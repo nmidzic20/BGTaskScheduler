7af9d39c6d026da3f760d77e0a4f462982b335dd
nmidzic20 <nmidzic20@foi.hr>
2022-11-27 14:25:23 +0100

    Initial Commit

---
+++ BG/BGApp.swift
@@ -0,0 +1,17 @@
+//
+//  BGApp.swift
+//  BG
+//
+//  Created by FOI on 27.11.2022..
+//
+
+import SwiftUI
+
+@main
+struct BGApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
